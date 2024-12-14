cc90e83be955de884cf4ba4d28453ea013050a81
valecc23 <134458072+valecc23@users.noreply.github.com>
2024-12-14 11:00:33 -0800

    Actualizacion del script

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -7,8 +7,9 @@ CREATE TABLE Usuario (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
-    rol ENUM('admin', 'vendedor', 'cliente') NOT NULL,
-    correo VARCHAR(100) UNIQUE NOT NULL
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    rol ENUM('admin', 'vendedor', 'cliente') NOT NULL
 );
 
 -- Tabla Producto
